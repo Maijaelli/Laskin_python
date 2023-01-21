cb483645b7df0917a766d425b69c1968d4f5e7f3
Maija Grönroos <maegro@Maija-MacBook-Air.local>
2023-01-21 11:40:32 +0200

    lisätty kaikki oleelliset funktiot

--- Laskin.py
+++ Laskin.py
@@ -2,21 +2,35 @@
 def lisaa(x, y):
     return x + y
 
+# Vähennysfunktio
+def vahenna(x, y):
+    return x - y
+
+# Jakofunktio
+def jako(x, y):
+    return x / y
+
+# Kertofunktio
+def kerto(x, y):
+    return x * y
+
 while True:
     #valikon valinnat
     print("-----------")
     print("Tervetuloa Maijan laskimeen!")
     print("Valitse vaihtoehdoista")
     print("1. lisää")
-    print("2. poistu")
+    print("2. vähennä")
+    print("3. jaa")
+    print("4. kerro")
     print("-----------")
 
-    valinta = input("valintasi (1/2): ")
+    valinta = input("valintasi (1/2/3/4): ")
 
 
     #riippuen käyttäjän valinnasta, mikä funktio kutsutaan käyttöön
 
-    if valinta in ['1','2']:
+    if valinta in ['1','2','3','4']:
         num1 = int(input("Anna ensimmäinen numero: "))
         num2 = int(input("Anna toinen numero: "))
 
@@ -24,6 +38,13 @@ while True:
             tulos = lisaa(num1, num2)
             print(tulos)
         elif valinta == '2':
-            print("Tämä on valinta kaksi.")
+            tulos = vahenna(num1, num2)
+            print(tulos)
+        elif valinta == '3':
+            tulos = jako(num1, num2)
+            print(tulos)
+        elif valinta == '4':
+            tulos = kerto(num1, num2)
+            print(tulos)
         else:
             print("Väärä valinta.")

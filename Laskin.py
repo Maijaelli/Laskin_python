89a73b66353e48ff6ef47077b24ab2f2d8dd2e88
Maija Grönroos <maegro@Maija-MacBook-Air.local>
2023-01-21 11:29:37 +0200

    lisäysfunktio lisätty

--- Laskin.py
+++ Laskin.py
@@ -1,9 +1,13 @@
+# Lisäysfunktio
+def lisaa(x, y):
+    return x + y
+
 while True:
     #valikon valinnat
     print("-----------")
     print("Tervetuloa Maijan laskimeen!")
     print("Valitse vaihtoehdoista")
-    print("1. jotain")
+    print("1. lisää")
     print("2. poistu")
     print("-----------")
 
@@ -13,8 +17,12 @@ while True:
     #riippuen käyttäjän valinnasta, mikä funktio kutsutaan käyttöön
 
     if valinta in ['1','2']:
+        num1 = int(input("Anna ensimmäinen numero: "))
+        num2 = int(input("Anna toinen numero: "))
+
         if valinta == '1':
-            print("Tämä on vaihtoehto yksi.")
+            tulos = lisaa(num1, num2)
+            print(tulos)
         elif valinta == '2':
             print("Tämä on valinta kaksi.")
         else:

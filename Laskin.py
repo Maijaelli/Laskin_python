00039f405782d514b2e2cdfb891bb058fb14d938
Maija Grönroos <maegro@Maija-MacBook-Air.local>
2023-01-21 12:06:41 +0200

    tulosten katselufunktio ja ohjelman lopetus lisätty

--- Laskin.py
+++ Laskin.py
@@ -14,6 +14,13 @@ def jako(x, y):
 def kerto(x, y):
     return x * y
 
+# Tulosten katselu-funktio
+def katso_tulokset():
+    with open("laskimen_tulokset.txt", "r") as f:
+        rivit = f.readlines()
+        for rivi in rivit[-5:]:
+            print(rivi)
+
 while True:
     #valikon valinnat
     print("-----------")
@@ -23,9 +30,11 @@ while True:
     print("2. vähennä")
     print("3. jaa")
     print("4. kerro")
+    print("5. katso 5 viimeisintä laskutoimitusta")
+    print("6. lopeta ohjelma")
     print("-----------")
 
-    valinta = input("valintasi (1/2/3/4): ")
+    valinta = input("valintasi (1/2/3/4/5/6): ")
 
 
     #riippuen käyttäjän valinnasta, mikä funktio kutsutaan käyttöön
@@ -51,7 +60,14 @@ while True:
         with open("laskimen_tulokset.txt", "a") as f:
             f.write(f"{num1} {op} {num2} = {tulos}\n")
         print(f"{num1} {op} {num2} = {tulos}")
-
+    
+    # Kutsutaan viisi viimeisintä laskutoimitusta
+    elif valinta == '5':
+        katso_tulokset()
+    
+    # Ohjelman lopetus
+    elif valinta == '6':
+        break
 
     else:
         print("Väärä valinta.")
